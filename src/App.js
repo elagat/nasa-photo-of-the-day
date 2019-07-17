3dff72b966c89ff4c65f634fbcbc58a7695b78a6
Emily Lagattolla <elagattolla@gmail.com>
2019-07-17 18:46:54 -0500

    limited console.log(res.data) with empty array

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <ApodGrid limit={10}/>
+      <ApodGrid limit={10} />
     </div>
   );
 }
--- src/components/ApodGrid.js
+++ src/components/ApodGrid.js
@@ -3,16 +3,18 @@ import axios from "axios";
 import ApodCard from "./ApodCard.js"
 
 function ApodGrid({ limit }) {
-  const [apod, setApod] = useState([]);
+  const [mediaUrl, setMediaUrl] = useState([])
 
   useEffect( () => {
-
     axios
-      .get(`https://api.nasa.gov/planetary/apod`)
+      .get(`https://api.nasa.gov/planetary/apod?api_key=FInBtIykxEFRHLUAbzXEjZVdcjP594JgzEPhWkQR`)
       .then(res => {
-        console.log(res);
+        setMediaUrl(res.data);
+        console.log(res.data);
       })
-  });
+      .catch(err => console.log(err));
+  }, []);
+
 
   return (
     <div className="apod-grid">Hello</div>
